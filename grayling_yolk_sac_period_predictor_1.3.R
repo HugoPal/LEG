1461fdbea68326b948ceb6169fef4a090bcf3bad
HugoPal <hugo_palejowski@hotmail.co.uk>
2021-10-20 11:56:28 +0200

    Update grayling_yolk_sac_period_predictor_1.3.R

--- grayling_yolk_sac_period_predictor_1.3.R
+++ grayling_yolk_sac_period_predictor_1.3.R
@@ -1,12 +1,17 @@
-### Tool to predict timing of the end of the yolk sac period, and fish length at that point, in grayling, based on length at hatching, how many days a fish took to hatch, and yolk sac volume at hatching.
-# DISCLAIMER: as with all predictive models, this tool will predict well if the input variables are within the range used to train the original model, outside of the below ranges predictions cannot be trusted:
+### Tool to predict timing of the end of the yolk sac period, and fish length at that point, in grayling, 
+# based on length at hatching, how many days a fish took to hatch, and yolk sac volume at hatching.
+# DISCLAIMER: as with all predictive models, this tool will predict well if the input variables are within 
+# the range used to train the original model, outside of the below ranges predictions cannot be trusted:
 #   - fish length at hatching: 8.419-10.677 mm
 #   - number of days taken to hatch: 30-38
 #   - yolk sac volume at hatching: 5.5-11.9 mm^3
 
 ### Data formatting:
 # data must be in format of one row per fish. 
-# The data must contain the following columns: "fish_ID", "incubation", "hatchlength", "hatchvolume", where fish_ID is an individual's unique ID, incubation is the number of days an individual took to hatch, hatlength is the length at hatching, and hatchvolume is the yolk sac volume at hatching.
+# The data must contain the following columns: "fish_ID", "incubation", "hatchlength", "hatchvolume", where fish_ID 
+#vis an individual's unique ID, incubation is the number of days an individual took to hatch, hatlength is the 
+# length at hatching, and hatchvolume is the yolk sac volume at hatching.
+
 # "fish_ID" must be coded as.character. "incubation", "hatchlength", "hatchvolume" must be as.numeric.
 # An example dataset is given here.
 if(!require(tidyverse)){
@@ -24,13 +29,13 @@ tibble("fish_ID" = c(seq(1,5,1)),
        "hatchvolume" =c(runif(5, min=5.5, max=11.9)))
 
 # FIRST load your data, it must follow data formatting given above
-DATAFRAME <- read_csv()
+DATAFRAME <- read_csv("data/DATAFRAME.csv")
 
 ### Run tool
 # NEXT load timing and length predictors ENTER THEIR LOCATIONS HERE
 # two timing predictors are available (models 1 and 2 from Palejowski et al.), you can choose which you would like to use
-timing_predictor <- readRDS(file = "C:/Users/hpalejow/Documents/Work/Uni Work/Lausanne PhD/2 Growth curve salmonids/Grayling/predictor_timing_of_yolk_period_end.rds") #ENTER THE LOCAITON OF predictor_timing_of_emergence.rds FILE
-length_predictor <- readRDS(file = "C:/Users/hpalejow/Documents/Work/Uni Work/Lausanne PhD/2 Growth curve salmonids/Grayling/predictor_length_at_yolk_period_end.rds") #ENTER THE LOCATION OF length_timing_of_emergence.rds FILE
+timing_predictor <- readRDS(file = "models/predictor_timing_of_yolk_period_end.rds") #ENTER THE LOCAITON OF predictor_timing_of_emergence.rds FILE
+length_predictor <- readRDS(file = "models/predictor_length_at_yolk_period_end.rds") #ENTER THE LOCATION OF length_timing_of_emergence.rds FILE
 
 # the predictor tool
 unique_fish_ids <- unique(DATAFRAME$fish_ID)
@@ -40,7 +45,7 @@ end_of_yolk_sac_period_results <-tibble("fish_ID"=unique_fish_ids,
 
 for(i in 1:length(unique_fish_ids)) {
   
-  ID_of_fish = unique_fish_ids[i]
+  current_fish_ID = unique_fish_ids[i]
   days_taken_to_hatch =DATAFRAME$incubation[i]
   yolk_sac_volume_at_hatching_mm3 =DATAFRAME$hatchvolume[i]
   fish_length_at_hatching_mm =DATAFRAME$hatchlength[i]
@@ -49,7 +54,7 @@ for(i in 1:length(unique_fish_ids)) {
   
   ## day of end of yolk-sac period (when yolk sac volume = 20% of volume at hatching - can change this threshold in the propYSrem= )
   time_plot_models_data_point<-tibble(
-    fish_ID=ID_of_fish,
+    fish_ID=current_fish_ID,
     incubation=days_taken_to_hatch,
     hatchvolume=yolk_sac_volume_at_hatching_mm3,
     hatchlength=fish_length_at_hatching_mm,
@@ -57,11 +62,11 @@ for(i in 1:length(unique_fish_ids)) {
   )
   
   time_plot_models_predict_point<-predict(timing_predictor, time_plot_models_data_point, allow.new.levels=T) 
-  end_of_yolk_sac_period_results$emergence_date[end_of_yolk_sac_period_results$fish_ID==ID_of_fish]<- time_plot_models_predict_point
+  end_of_yolk_sac_period_results$emergence_date[end_of_yolk_sac_period_results$fish_ID==current_fish_ID]<- time_plot_models_predict_point
   
   ## length at end of yolk-sac period (when yolk sac volume = 20% of volume at hatching)
   length_plot_models_data_point<-tibble(
-    fish_ID=ID_of_fish,
+    fish_ID=current_fish_ID,
     incubation=days_taken_to_hatch,
     hatchvolume=yolk_sac_volume_at_hatching_mm3,
     hatchlength=fish_length_at_hatching_mm,
@@ -70,47 +75,46 @@ for(i in 1:length(unique_fish_ids)) {
   )
   
   length_plot_models_predict_point<-predict(length_predictor, length_plot_models_data_point, allow.new.levels=T) 
-  end_of_yolk_sac_period_results$emergence_length[end_of_yolk_sac_period_results$fish_ID==ID_of_fish] <- length_plot_models_predict_point
+  end_of_yolk_sac_period_results$emergence_length[end_of_yolk_sac_period_results$fish_ID==current_fish_ID] <- length_plot_models_predict_point
   
 }
 
 # get results
 end_of_yolk_sac_period_results
-
 range(end_of_yolk_sac_period_results$emergence_date)
 range(end_of_yolk_sac_period_results$emergence_length)
-hist(end_of_yolk_sac_period_results$emergence_date)
-hist(end_of_yolk_sac_period_results$emergence_length)
+
 
 # merge results with dataset
-merge(DATAFRAME, end_of_yolk_sac_period_results, by = "fish_ID", all=T)
+DATAFRAME<-merge(DATAFRAME, end_of_yolk_sac_period_results, by = "fish_ID", all=T)
+write.csv(DATAFRAME,
+          file = "outputs/DATAFRAME_predicted.csv")
 
 # plot results
-DATAFRAME_fish_emergence_histos<-ggarrange(
-  ggplot(tibble(end_of_yolk_sac_period_results$emergence_date), aes(end_of_yolk_sac_period_results$emergence_date)) +
-    geom_histogram(binwidth = 0.05,
-                   col="black",
-                   fill="grey") +
-    xlab("Time to end of yolk-sac period (days post-fertilisation)") +
-    theme(axis.line = element_line(size = 0.5, linetype = "solid", colour = "black"),
-          panel.grid = element_blank(),
-          panel.background = element_blank(),
-          axis.title = element_text(size=12),
-          panel.border = element_rect(colour = "black", fill=NA, size=1)) +
-    scale_x_continuous(breaks = seq(58, 65, by = 0.1), limits = c(58,65)),
-  ggplot(tibble(end_of_yolk_sac_period_results$emergence_length), aes(end_of_yolk_sac_period_results$emergence_length)) +
-    geom_histogram(binwidth = 0.1,
-                   col="black",
-                   fill="grey") +
-    xlab("Length at end of yolk-sac period (mm)") +
-    theme(axis.line = element_line(size = 0.5, linetype = "solid", colour = "black"),
-          panel.grid = element_blank(),
-          panel.background = element_blank(),
-          axis.title = element_text(size=12),
-          panel.border = element_rect(colour = "black", fill=NA, size=1)) +
-    scale_x_continuous(breaks = seq(11, 15, by = 0.2), limits = c(12,14.5)),
-  ncol=1,
-  labels = "AUTO")
-DATAFRAME_fish_emergence_histos
+hist_larval_times <- hist(end_of_yolk_sac_period_results$emergence_date, 
+                          breaks = seq(min(end_of_yolk_sac_period_results$emergence_date), max(end_of_yolk_sac_period_results$emergence_date), length.out = 7),
+                          main = "timing of larval period end",
+                          xlab = "days post-fertilisation")
+
+hist_larval_lengths <- hist(end_of_yolk_sac_period_results$emergence_length, 
+                            breaks = seq(min(end_of_yolk_sac_period_results$emergence_length), max(end_of_yolk_sac_period_results$emergence_length), length.out = 7),
+                            main = "length at larval period end",
+                            xlab = "length (mm)")
+
+jpeg(file="outputs/histograms_of_predicted_times.png")
+hist(end_of_yolk_sac_period_results$emergence_date, 
+     breaks = seq(min(end_of_yolk_sac_period_results$emergence_date), max(end_of_yolk_sac_period_results$emergence_date), length.out = 7),
+     main = "timing of larval period end",
+     xlab = "days post-fertilisation")
+dev.off()
+
+jpeg(file="outputs/histograms_of_predicted_lengths.png")
+hist(end_of_yolk_sac_period_results$emergence_length, 
+     breaks = seq(min(end_of_yolk_sac_period_results$emergence_length), max(end_of_yolk_sac_period_results$emergence_length), length.out = 7),
+     main = "length at larval period end",
+     xlab = "length (mm)")
+dev.off()
+
+
 
 
